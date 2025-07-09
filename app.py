894f00ddaa99eb9bc45b01f01a95c9a1aed4054d
Devansh Gupta <devanshgupta@Devanshs-MacBook-Air.local>
2025-07-10 00:13:45 +0530

    Use environment PORT for production deployment

--- app.py
+++ app.py
@@ -77,4 +77,6 @@ with app.app_context():
 from routes import *
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
